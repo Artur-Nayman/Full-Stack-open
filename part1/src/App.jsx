0125f1d44b7a14b8ab59b2d712f9ebc358dc3d67
Artur Nayman <arturnaiman@gmail.com>
2025-09-12 13:22:16 +0300

    1.2 done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,4 @@
 const App = () => {
-
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -8,14 +7,14 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
-
   const Header = ({ course }) => <h1>{course}</h1>
+  const Part = ({ part, exercises }) => <p>{part} {exercises}</p>
   const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => (
-    <>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
-    </>
+    <div>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
+    </div>
   )
   const Total = ({ exercises1, exercises2, exercises3 }) => (
     <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
